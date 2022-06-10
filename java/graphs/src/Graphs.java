d696521ed27130011db8f585b565ad884642f7fa
adarsh <adarshofficial11@gmail.com>
2022-06-10 22:44:28 +0530

    basic class structure for graph algorithms

---
+++ java/graphs/src/Graphs.java
@@ -0,0 +1,11 @@
+package graphs.src;
+
+import java.io.*;
+
+// Java program to implement
+// a Simple Graph
+public class Graphs {
+    public static void main(String[] args) {
+        System.out.println("Graph Implementation");
+    }
+}
