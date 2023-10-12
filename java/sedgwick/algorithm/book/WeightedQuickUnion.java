33add7b72cbeb850be483e35a67edc3f6a202e73
Adarsh <adarshofficial11@gmail.com>
2023-10-12 08:44:16 +0530

    weighted quick union with path compression.

--- java/sedgwick/algorithm/book/WeightedQuickUnion.java
+++ java/sedgwick/algorithm/book/WeightedQuickUnion.java
@@ -2,25 +2,33 @@ package sedgwick.algorithm.book;
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+/****
+ *  Compilation:  javac UF.java
+ *  Execution:    java UF < input.txt
+ *  Dependencies: StdIn.java StdOut.java
+ *  Data files:   https://algs4.cs.princeton.edu/15uf/tinyUF.txt
+ *                https://algs4.cs.princeton.edu/15uf/mediumUF.txt
+ *                https://algs4.cs.princeton.edu/15uf/largeUF.txt
+ **/
 
-public class QuickUnion {
-    private int[] id;
+public class WeightedQuickUnion {
+    private final int[] id;
+    private final int[] sz;
     private int count; //number of components (connected entities)
-    public QuickUnion(int N){
+    public WeightedQuickUnion(int N){
         // initialize N sites with integer names (0 to N-1)
         count = N;
         id = new int[N];
+        sz = new int[N];
         for (int i = 0; i < N; i++) {
             id[i] = i; //initial assumption is that each point is its own component.
         }
+        for (int i = 0; i <N; i++) {
+            sz[i]= 1;// at the start each point has size 1 since it is its own tree.
+        }
     }
     /*
-        Quick Union has optimized union and find methods.
-        Worst case: we need to call union N-1 times.
-        Each union calls find() twice which eventually will need to traverse
-        the entire tree if we have just a single branched tree.
-        Over all this has time complexity little faster than quadratic.
-        Improvement of this algorithm is weighted quick union.
+        Weighted Quick Union has optimized union method.
      */
     public void union(int p, int q){
         // simply attaching p to q will not work as we need to move the entire branch of p and connect to q's root.
@@ -29,16 +37,30 @@ public class QuickUnion {
         if(rootQ == rootP){
             return; // important: to prevent cycle formation.
         }
-        //make the root of p point to root of q:
-        id[rootP] = rootQ;
+        //compare the sizes and then attach the smaller under the larger
+        if(sz[rootP]<=sz[rootQ]){
+            //make the root of p point to root of q:
+            id[rootP] = rootQ;
+            sz[rootQ] += sz[rootP];//new
+        }else {
+            id[rootQ]=rootP;
+            sz[rootP]+=sz[rootQ];//new
+        }
+
         // the number of components reduces by 1 here also:
         count--;
     }
 
+    /*
+    Path compression can be applied to the find() method for more optimization.
+     */
     public int find(int p){
         // returns the root node site name/number.
         while(p!= id[p]){
-            p = id[p]; // move up the chain.
+            //PATH COMPRESSION:
+            //make the node point to the grandparent node.
+            id[p] = id[id[p]];
+            p = id[p]; // move up the new chain.
         }
         return p;//or id[p] since both are same at the root node.
     }
@@ -56,12 +78,15 @@ public class QuickUnion {
     { // Solve dynamic connectivity problem on StdIn.
         int N = StdIn.readInt();
         // Read number of sites.
-        QuickUnion uf = new QuickUnion(N);
+        WeightedQuickUnion uf = new WeightedQuickUnion(N);
         // Initialize N components.
         while (!StdIn.isEmpty())
         {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
+            if (p < 0 || q < 0) {
+                break;
+            }
             // Read pair to connect.
             if (uf.connected(p, q)) continue; // Ignore if connected.
             uf.union(p, q);
